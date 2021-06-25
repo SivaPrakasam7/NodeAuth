ceecdaf2eb5286e11dfd7c7950a2a9b28f3e5ad0
SivaPrakasam7 <prakasams22@gmail.com>
2021-06-25 05:55:16 -0400

    mod

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 
 var express = require('express'),
     path = require('path'),
-    MongoClient = require('mongodb').MongoClient,
+    mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
     bcrypt = require('bcrypt'),
     passport = require('passport'),
     GoogleStrategy = require('passport-google-oauth2').Strategy,
     url = 'mongodb+srv://siva:siva12345@cluster0.yudpn.mongodb.net/nodelogin?retryWrites=true&w=majority';
+
+// Mongoose configuration
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+var con = mongoose.connection;
+const User = mongoose.Schema({
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    mobileNo: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /d{10}/.test(v);
+            },
+            message: '{VALUE} is not valid number'
+        }
+    }, address: { type: String, required: true }, about: { type: String }
+});
+UserTable = mongoose.model('users', User);
+
+// app init
 
 const app = express();
 const port = 3000;
@@ -14,11 +36,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(require('express-session')({
-    secret: '*&@%VUGHCBIW',
-    resave: false,
-    saveUninitialized: true
-}));
+app.use(require('express-session')({ secret: '*&@%VUGHCBIW', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -31,18 +49,12 @@ passport.use(new GoogleStrategy({
     callbackURL: 'https://b8cab82c82fd.ngrok.io/auth/google/callback',
     passReqToCallback: true,
 },
-    function (request, accessToken, refreshToken, profile, done) {
-        return done(null, profile);
-    }
+    function (request, accessToken, refreshToken, profile, done) { return done(null, profile); }
 ));
 
-passport.serializeUser(function (user, done) {
-    done(null, user);
-});
+passport.serializeUser(function (user, done) { done(null, user); });
 
-passport.deserializeUser(function (user, done) {
-    done(null, user);
-});
+passport.deserializeUser(function (user, done) { done(null, user); });
 
 // ROUTES
 
@@ -62,62 +74,38 @@ app.get('/', function (req, res) {
 // Normal login
 
 app.post('/login', function (req, res) {
-    var username = req.body.email;
     var password = req.body.passwd;
-    MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db('nodelogin');
-        dbo.collection('users').findOne({ name: username }, function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            try {
-                bcrypt.compare(password, result.password, function (err, isPasswordMatch) {
-                    if (err) throw err;
-                    if (isPasswordMatch) {
-                        // bcrypt.genSalt(5,function(err,salt){
-                        //     if (err) throw err;
-                        //     bcrypt.hash(String(result._id),salt,function(err,sid){
-                        //         if (err) throw err;
-                        //         req.session.userID=sid;
-                        //     });
-                        // });
-                        req.session.userID = result._id;
-                        res.redirect('/')
-                    } else res.send('Invalid credential')
-                });
-            } catch (err) {
-                res.send('Invalid')
-            }
-            db.close();
-        });
+    UserTable.findOne({ name: req.body.email }, function (err, result) {
+        try {
+            bcrypt.compare(password, result.password, function (err, isPasswordMatch) {
+                if (err) throw err;
+                if (isPasswordMatch) {
+                    req.session.userID = result._id;
+                    res.redirect('/')
+                } else res.send('Invalid credential')
+            });
+        } catch (err) {
+            res.send('Invalid')
+        }
     });
 });
 
-app.get('/register', function (req, res) {res.render('register')});
+app.get('/register', function (req, res) { res.render('register') });
 
 app.post('/register', function (req, res) {
     var password = req.body.passwd;
     bcrypt.genSalt(10, function (err, salt) {
         if (err) throw err;
         bcrypt.hash(password, salt, function (err, hash) {
-            MongoClient.connect(url,{ useUnifiedTopology: true }, function (err, db) {
-                if (err) throw err;
-                var dbo = db.db('nodelogin');
-                dbo.collection('users').insertOne({ name: req.body.username, password: hash,mobileNo:req.body.mobileNo ,address:req.body.address,about:req.body.about}, function (err, res) {
-                    if (err) throw err;
-                    console.log(res.insertedId);
-                    req.session.userID=res.insertedId;
-                    db.close();
-                });
-                res.redirect('/');
+            con.collection('users').insertOne({ name: req.body.username, password: hash, mobileNo: req.body.mobileNo, address: req.body.address, about: req.body.about }, function (err, result) {
+                req.session.userID = result.insertedId;
             });
+            res.redirect('/');
         });
     });
 });
 
-app.get('/login', function (req, res) {
-    res.render('login')
-});
+app.get('/login', function (req, res) { res.render('login') });
 
 app.get('/logout', function (req, res) {
     req.logout();
@@ -125,6 +113,4 @@ app.get('/logout', function (req, res) {
     res.redirect('/');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
+app.listen(port, () => { console.log(`Example app listening at http://localhost:${port}`) });
